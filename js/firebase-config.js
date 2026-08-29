175bb6cd664c1f20bfe9a54328a4614fde8705bf
miloscermak <extracermak@gmail.com>
2026-08-29 18:04:46 +0200

    Add Firebase project config and allow comments in database rules

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,9 +2,11 @@
 // Tyhle údaje nejsou tajné, v prohlížeči je vidí každý. Bezpečnost stojí
 // na pravidlech v database.rules.json, ne na jejich utajení.
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  appId: ""
+  apiKey: "AIzaSyCmXWQG_T8vu5-OraFuKmfra_sCPVwauhE",
+  authDomain: "hlasovatko-5d8b1.firebaseapp.com",
+  databaseURL: "https://hlasovatko-5d8b1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hlasovatko-5d8b1",
+  storageBucket: "hlasovatko-5d8b1.firebasestorage.app",
+  messagingSenderId: "1000159049411",
+  appId: "1:1000159049411:web:452bb9bbe2a0ec570f9476"
 };
